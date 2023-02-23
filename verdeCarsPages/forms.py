a5c5513ba2c883b602a914cb96a49cbe53658816
Ann Marie Humble <A02324311@aggies.usu.edu>
2023-02-23 15:58:37 -0700

    Potentially got the form working, but I changed the User class and now it has an error, prolly I need to make some migrations

---
+++ verdeCarsPages/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import User, Car
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['fname', 'lname', 'usernm', 'passwd']
--- verdeCarsPages/views.py
+++ verdeCarsPages/views.py
@@ -6,6 +6,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from datetime import datetime
-# Create your views here.
+from .forms import UserForm
+
+
 def index(request):
     return render(request, 'verdeCarsPages/index.html')
+
+def login(request):
+    if request.method == "POST":
+        # If the user posts something, either take what they filled out
+        # and turn it into a Form object, or if the form was empty, pass in None
+        form = UserForm(request.POST or None)
+        if form.is_valid():
+            form.save
+        return render(request, 'verdeCarsPages/join.html', {})
+        
+    else:
+        return render(request, 'verdeCarsPages/login.html', {})
