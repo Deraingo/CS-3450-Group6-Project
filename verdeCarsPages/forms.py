9c125a4bd9c3d16e96acaecffd54fdbf04899bba
Ann Marie Humble <A02324311@aggies.usu.edu>
2023-02-25 12:26:18 -0700

    basic recognition of username and re-direction is implemented, but it still needs to access the databse.

--- verdeCarsPages/forms.py
+++ verdeCarsPages/forms.py
@@ -5,4 +5,4 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['fname', 'lname', 'usernm', 'passwd']
-        exclude = ['userType', 'money']
+        exclude = ['userType', 'money']  # These have default values and are not entered when creating an account
--- verdeCarsPages/views.py
+++ verdeCarsPages/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from datetime import datetime
+from .models import User, Car
 from .forms import UserForm
 
 
@@ -13,13 +14,12 @@ def index(request):
     return render(request, 'verdeCarsPages/index.html')
 
 def login(request):
+    allUsers = User.objects.all
     if request.method == "POST":
-        # If the user posts something, either take what they filled out
-        # and turn it into a UserForm object, or if the form was empty, pass in None
-        form = UserForm(request.POST or None)  
+        form = UserForm(request.POST or None)  # Pass in 'None' if the user entered nothing into the form
         if form.is_valid():
             form.save()
-        return render(request, 'verdeCarsPages/login.html', {})
+        return render(request, 'verdeCarsPages/login.html', {'all': allUsers})  # Pass allUsers into the page so that it can be used for authentication later
         
     else:
         return render(request, 'verdeCarsPages/login.html', {})
