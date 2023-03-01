b0cf30e1f974423f9f5d447e087f34c6f65ef66d
Deraingo <42049596+Deraingo@users.noreply.github.com>
2023-03-01 16:20:04 -0700

    added comma where the merge conflict resolver removed it

--- verdeCarsPages/urls.py
+++ verdeCarsPages/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('reserve-car/', views.reservecar, name='reservecar'),
-    path('checkout-confirmation/', views.checkoutConfirmation, name='checkoutConfirmation')
+    path('checkout-confirmation/', views.checkoutConfirmation, name='checkoutConfirmation'),
     path('retrievalPage/', views.retrievalPage, name='retrievalPage'),
     path('catalog/', views.catalog, name='catalog'),
 ]
