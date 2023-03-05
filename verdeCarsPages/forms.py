026293bcf7fbea7fb53e3f11de09a1198067542b
Ann Marie Humble <A02324311@aggies.usu.edu>
2023-03-04 18:27:55 -0700

    made two forms; handled them in view; prolly doesn't work yet

--- verdeCarsPages/forms.py
+++ verdeCarsPages/forms.py
@@ -2,11 +2,14 @@ from django import forms
 from .models import User, Car
 
 class UserForm(forms.ModelForm):
+    create_user = forms.BooleanField(initial=True)
     class Meta:
         model = User
         fields = ['fname', 'lname', 'usernm', 'passwd']
         exclude = ['userType', 'money']  # These have default values and are not entered when creating an account
 
 class LoginForm(forms.Form):
+    enter_login = forms.BooleanField(initial=True)
+
     class Meta:
         fields = ['LoginUsername', 'LoginPassword']
--- verdeCarsPages/views.py
+++ verdeCarsPages/views.py
@@ -7,19 +7,41 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from datetime import datetime
 from .models import User, Car
-from .forms import UserForm
+from .forms import UserForm, LoginForm
 
 
 def index(request):
     return render(request, 'verdeCarsPages/index.html')
 
-def login(request):
+def login(request, form_id):
     allUsers = User.objects.all
-    if request.method == "POST":
-        form = UserForm(request.POST or None)  # Pass in 'None' if the user entered nothing into the form
-        if form.is_valid():
-            form.save()
-        return render(request, 'verdeCarsPages/login.html', {'all': allUsers})  # Pass allUsers into the page so that it can be used for authentication later
 
-    else:
-        return render(request, 'verdeCarsPages/login.html', {'all': allUsers})
+    user_form = UserForm()
+    login_form = LoginForm()
+    if request.method == "POST":
+        if 'create_user' in request.POST:
+            user_form = UserForm()
+            if user_form.is_valid():
+                user_form.save()
+                return render(request, 'verdeCarsPages/login.html', {'all': allUsers})
+        # if 'enter_login' in request.POST:
+        #     login_form = LoginForm()
+        #     if login_form.is_valid():
+        #         login_form.save()
+        #         return render(request, 'verdeCarsPages/login.html', {'all': allUsers})
+
+    context = {
+        'create_user_form': user_form,
+        'login_form': login_form,
+    }
+        # form = LoginForm(request.POST or None)
+        # if form.is_valid():
+        #     form.save()
+        # form = UserForm(request.POST or None)  # Pass in 'None' if the user entered nothing into the form
+        # if form.is_valid():
+        #     form.save()
+    #     return render(request, 'verdeCarsPages/login.html', {'all': allUsers})  # Pass allUsers into the page so that it can be used for authentication later
+
+    # else:
+    
+    return render(request, 'verdeCarsPages/login.html', context=context)
