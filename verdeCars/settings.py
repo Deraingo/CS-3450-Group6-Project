af4a4f6f507efe7c8e6aeb03e111b373036c79ca
Deraingo <deraingo3@gmail.com>
2023-03-24 13:16:24 -0600

    git made me add this so I removed secret key and commited

--- verdeCars/settings.py
+++ verdeCars/settings.py
@@ -24,14 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #test
-# SECRET_KEY = 
+# SECRET_KEY
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/verdeCarsPages/media')
 
 
 # Application definition
