df1ed3639fba655c354d8e8a6112bc8c850a71aa
SarahNGardner <sarahngardner@gmail.com>
2023-03-28 00:20:49 -0600

    inserted form details

--- verdeCarsPages/forms.py
+++ verdeCarsPages/forms.py
@@ -24,3 +24,23 @@ class LoginForm(forms.ModelForm):
         model = User
         fields = ['usernm', 'passwd']
         exclude = ['fname', 'lname', 'userType', 'money']
+        
+class RentCarForm(forms.ModelForm):
+        #rental_day = forms.DateField()
+        #rental_money = forms.FloatField()
+        #address = forms.CharField()
+        #car_cost=forms.FloatField()
+        
+        class Meta:
+            model = User
+            fields = []
+            exclude = ['fname', 'lname', 'userType', 'usernm', 'passwd','money']
+        
+        #def clean_payment_amount(self):
+            #data = self.cleaned_data['rental_money']
+            #if data < car_cost:
+             #   raise ValidationError(_('Not enough'))
+                
+           # return data
+        
+        
